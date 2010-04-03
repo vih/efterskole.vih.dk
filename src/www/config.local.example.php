f019bb4c7f9f2cbed17fe168004d3704b5ade778
Lars Olesen <lars@intraface.dk>
2010-04-03 06:30:28 +0000

    Added config example.

---
+++ src/www/config.local.example.php
@@ -0,0 +1,5 @@
+<?php
+ini_set('include_path', dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
+
+$GLOBALS["intraface_private_key"] = 'ch8LGZYKtKgAmeVhKRDDrtT9awjuuqcxParmZyeArDhCHk2ijv4';
+$GLOBALS["intraface_site_id"] = 26;
